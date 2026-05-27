0328138e5da8f862a9b179f3b5dec8ff0c7470f4
burnedinthesky <git@kuo.is>
2026-05-27 10:13:37 +0800

    feat: remove index page placeholder

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,10 +4,6 @@ import ChatPage from "./features/chat/pages/ChatPage";
 
 export const router = createBrowserRouter([
     {
-        path: "/",
-        element: <div>Hello World</div>,
-    },
-    {
         path: "/course/:id",
         element: <GeneticsCourse />,
     },
